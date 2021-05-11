612d5618622505a781beeb73f3935b974625a282
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2021-05-11 14:30:08 +1200

    Mouse works

--- Phase 1 code/seven_seg.vhd
+++ Phase 1 code/seven_seg.vhd
@@ -17,16 +17,16 @@ architecture arc of seven_seg is
     process (digit)
       begin
           case digit is
-            when "0000" => LED_out <= "0000001"; --0
-            when "0001" => LED_out <= "1001111"; --1
-            when "0010" => LED_out <= "0010010"; --2
-            when "0011" => LED_out <= "0000110"; --3
-            when "0100" => LED_out <= "1001100"; --4
-            when "0101" => LED_out <= "0100100"; --5  
-            when "0110" => LED_out <= "0100000"; --6
-            when "0111" => LED_out <= "0001111"; --7
+            when "0000" => LED_out <= "1000000"; --0
+				when "0001" => LED_out <= "1111001"; --1
+				when "0010" => LED_out <= "0100100"; --2
+            when "0011" => LED_out <= "0110000"; --3
+				when "0100" => LED_out <= "0011001"; --4
+            when "0101" => LED_out <= "0010010"; --5
+				when "0110" => LED_out <= "0000010"; --6		
+            when "0111" => LED_out <= "1111000"; --7
             when "1000" => LED_out <= "0000000"; --8
-            when "1001" => LED_out <= "0000100"; --9
+            when "1001" => LED_out <= "0010000"; --9
             when others => LED_out <= "1111111";
           end case;
       end process;
