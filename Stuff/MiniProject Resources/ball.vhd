ef417f0a781f1b167a773e821dcbd53642595426
UoA-sjer215 <68827415+UoA-sjer215@users.noreply.github.com>
2021-05-13 14:32:42 +1200

    Pipes done

--- Stuff/MiniProject Resources/ball.vhd
+++ Stuff/MiniProject Resources/ball.vhd
@@ -23,12 +23,12 @@ BEGIN
 size <= CONV_STD_LOGIC_VECTOR(40,10);
 height <= CONV_STD_LOGIC_VECTOR(480,10);
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
-ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,10);
-ball_y_pos <= CONV_STD_LOGIC_VECTOR(350,10);
+ball_x_pos <= CONV_STD_LOGIC_VECTOR(0,10);
+ball_y_pos <= CONV_STD_LOGIC_VECTOR(0,10);
 
 
-ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + height) and ('0' & pixel_column <= ball_x_pos + height) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + size) and ('0' & pixel_column <= ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & ball_y_pos <= pixel_row + height) and ('0' & pixel_row <= ball_y_pos + height) )  else	-- y_pos - size <= pixel_row <= y_pos + size
 			'0';
 
 
