a81bc1962e5ab39ae0ca12f509de73361e726974
uoa-ppat965 <68896686+uoa-ppat965@users.noreply.github.com>
2021-05-26 15:13:53 +1200

    Update coin.vhd

--- FinalGameDesign/GameDesignCode/coin.vhd
+++ FinalGameDesign/GameDesignCode/coin.vhd
@@ -23,6 +23,7 @@ SIGNAL coin_x_motion								: std_logic_vector(9 DOWNTO 0);
 Signal send											: std_logic_vector(3 downto 0) := "0001";
 signal lfsr1 										: std_logic_vector(3 downto 0);
 signal count										: std_logic_vector(9 downto 0);
+signal iscoin										: std_logic;
 
 BEGIN           
 
@@ -33,7 +34,7 @@ port map (Clk => clk, reset => '1', seed => send, lfsr => lfsr1);
 -- pipe_x_pos and pipe_y_pos show the (x,y) for the centre of pipe
 
 coin_on <= '1' when ( ('0' & coin_x_pos <= pixel_column + radius) and ('0' & pixel_column <= coin_x_pos + radius) 	-- x_pos - radius <= pixel_column <= x_pos + radius
-					and ('0' & coin_y_pos <= pixel_row + radius) and ('0' & pixel_row <= coin_y_pos + radius) )  else	-- y_pos - radius <= pixel_row <= y_pos + radius
+					and ('0' & coin_y_pos <= pixel_row + radius) and ('0' & pixel_row <= coin_y_pos + radius) and (iscoin='1'))  else	-- y_pos - radius <= pixel_row <= y_pos + radius
 			'0';					
 			
 Move_coin: process (vert_sync) 
@@ -48,36 +49,36 @@ begin
 				count <= "0000000000";
 		-- Bounce off top or bottom of the scree
 		elsif (('0' & coin_x_pos <= CONV_STD_LOGIC_VECTOR(0,11))) then 
-			--coin_on <= '0';
-			case lfsr1 is
-				when "0001" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(30,10);
-				when "0010" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(60,10);
-				when "0011" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(90,10);
-				when "0100" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(120,10);
-				when "0101" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(150,10);
-				when "0110" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(180,10);
-				when "0111" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(210,10);
-				when "1000" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(240,10);
-				when "1001" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(270,10);
-				when "1010" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(300,10);
-				when "1011" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(330,10);
-				when "1100" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(360,10);
-				when "1101" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(420,10);
-				when "1110" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(450,10);
-				when others => coin_y_pos <= CONV_STD_LOGIC_VECTOR(460,10);
-			end case;
-			if (send >= "1111") then
-				send <= "0001";
-			else
-				send <= send + "0001";
-			end if;
-			
-			if (count < "100101100") then
+			if (count < CONV_STD_LOGIC_VECTOR(300,9)) then
+				iscoin<= '0';
 				count <= count + "000000001";
 			else
-				--coin_on <= '1';
-				coin_x_pos <= CONV_STD_LOGIC_VECTOR(800,10);
-				count <= "0000000000";
+					case lfsr1 is
+						when "0001" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(30,10);
+						when "0010" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(60,10);
+						when "0011" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(90,10);
+						when "0100" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(120,10);
+						when "0101" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(150,10);
+						when "0110" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(180,10);
+						when "0111" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(210,10);
+						when "1000" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(240,10);
+						when "1001" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(270,10);
+						when "1010" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(300,10);
+						when "1011" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(330,10);
+						when "1100" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(360,10);
+						when "1101" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(420,10);
+						when "1110" => coin_y_pos <= CONV_STD_LOGIC_VECTOR(450,10);
+						when others => coin_y_pos <= CONV_STD_LOGIC_VECTOR(460,10);
+					end case;
+					
+					if (send >= "1111") then
+						send <= "0001";
+					else
+						send <= send + "0001";
+					end if;
+					iscoin <= '1';
+					coin_x_pos <= CONV_STD_LOGIC_VECTOR(980,10);
+					count <= "0000000000";
 			end if;
 		else 
 			coin_x_pos <= coin_x_pos - coin_x_motion;
