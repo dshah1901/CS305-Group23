8953bcf84b14858ddc2666eb63e2e4cbb4a7ad8a
dshah1901 <dhvanishah192001@gmail.com>
2021-05-26 16:11:41 +1200

    Update text_display.vhd

--- FinalGameDesign/GameDesignCode/text_display.vhd
+++ FinalGameDesign/GameDesignCode/text_display.vhd
@@ -181,19 +181,7 @@ architecture behaviour of text_display is
 					   font_col <= pixel_column(5 downto 3); -- To change the font size
 						font_row <= pixel_row(5 downto 3);
 						if ((128 <= pixel_row) and (pixel_row < 192) and (128 <= pixel_column) and (pixel_column < 192)) then 
-							character_address <= conv_std_logic_vector(20,6); -- T
-							charOn <= '1';
-						elsif ((128 <= pixel_row) and (pixel_row < 192) and (192 <= pixel_column) and (pixel_column < 256)) then 
-							character_address <= conv_std_logic_vector(18,6); -- R
-							charOn <= '1';
-						elsif ((128 <= pixel_row) and (pixel_row < 192) and (256 <= pixel_column) and (pixel_column < 320)) then 
-							character_address <= conv_std_logic_vector(1,6); -- A
-							charOn <= '1';
-						elsif ((128 <= pixel_row) and (pixel_row < 192) and (320 <= pixel_column) and (pixel_column < 384)) then 
-							character_address <= conv_std_logic_vector(9,6); -- I
-							charOn <= '1';
-						elsif ((128 <= pixel_row) and (pixel_row < 192) and (384 <= pixel_column) and (pixel_column < 449)) then 
-							character_address <= conv_std_logic_vector(14,6); -- N
+							character_address <= conv_std_logic_vector(32,6); -- T
 							charOn <= '1';
 						else
 							character_address <= conv_std_logic_vector(32,6); -- Space 
