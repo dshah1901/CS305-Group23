74d5cc523d2c54493dfc2a796d81a2be1347c986
uoa-ppat965 <68896686+uoa-ppat965@users.noreply.github.com>
2021-05-26 15:29:00 +1200

    Update coin.vhd

--- FinalGameDesign/GameDesignCode/coin.vhd
+++ FinalGameDesign/GameDesignCode/coin.vhd
@@ -31,7 +31,7 @@ radius <= CONV_STD_LOGIC_VECTOR(10, 10);
 
 random: LFSR_generator
 port map (Clk => clk, reset => '1', seed => send, lfsr => lfsr1);
--- pipe_x_pos and pipe_y_pos show the (x,y) for the centre of pipe
+-- coin_x_pos and coin_y_pos show the (x,y) for the centre of coin
 
 coin_on <= '1' when ( ('0' & coin_x_pos <= pixel_column + radius) and ('0' & pixel_column <= coin_x_pos + radius) 	-- x_pos - radius <= pixel_column <= x_pos + radius
 					and ('0' & coin_y_pos <= pixel_row + radius) and ('0' & pixel_row <= coin_y_pos + radius) and (iscoin='1'))  else	-- y_pos - radius <= pixel_row <= y_pos + radius
@@ -40,12 +40,12 @@ coin_on <= '1' when ( ('0' & coin_x_pos <= pixel_column + radius) and ('0' & pix
 Move_coin: process (vert_sync) 
  	
 begin
-	-- Move pipe once every vertical sync
+	-- Move coin once every vertical sync
 	if (rising_edge(vert_sync)) then
 		coin_x_motion <= CONV_STD_LOGIC_VECTOR(2,10);
 		
 		if reset = '1' then
-				coin_x_pos <= CONV_STD_LOGIC_VECTOR(850,10);
+				coin_x_pos <= CONV_STD_LOGIC_VECTOR(750,10);
 				count <= "0000000000";
 		-- Bounce off top or bottom of the scree
 		elsif (('0' & coin_x_pos <= CONV_STD_LOGIC_VECTOR(0,11))) then 
@@ -83,7 +83,7 @@ begin
 		else 
 			coin_x_pos <= coin_x_pos - coin_x_motion;
 		end if;
-		-- Compute next pipe Y position
+		-- Compute next coin Y position
 		
 	end if;
 end process Move_coin;
