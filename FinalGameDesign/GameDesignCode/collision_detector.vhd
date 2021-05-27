8c8634f323250c7b1f309d487066264a95c23fa0
uoa-ppat965 <68896686+uoa-ppat965@users.noreply.github.com>
2021-05-27 14:37:35 +1200

    Made changes

--- FinalGameDesign/GameDesignCode/collision_detector.vhd
+++ FinalGameDesign/GameDesignCode/collision_detector.vhd
@@ -28,10 +28,10 @@ architecture behaviour of collision_detector is
   
  begin
     
-	 f_damage1 <= damage2 and damage1;
-	 f_damage2 <= damage3 and damage4;
+	 f_damage1 <= damage2;
+	 f_damage2 <= damage1;
 	 f_damage3 <= '0';
-	 death <= f_death;
+	 death <= '0';
 	 score <= f_score;
 	 score_ones <= f_score_ones;
 	 score_tens <= f_score_tens;
@@ -47,24 +47,25 @@ if (rising_edge(vert_sync)) then
 	if (reset <= '1') then
 		f_score_tens <= "0000";
 		f_score_ones <= "0000";
+		f_score <= conV_STD_LOGIC_VECTOR(0,7);
 	end if;
-	if( (gap1_lc <= CONV_STD_LOGIC_VECTOR(28,11)) and (CONV_STD_LOGIC_VECTOR(42,11) <= gap1_lc + CONV_STD_LOGIC_VECTOR(40,11) )) then --ga1_1c 
+	if( (gap1_lc <= CONV_STD_LOGIC_VECTOR(58,11)) and (CONV_STD_LOGIC_VECTOR(42,11) <= gap1_lc + CONV_STD_LOGIC_VECTOR(60,11) )) then --ga1_1c 
 		damage1 <='1';
 	else
 		damage1 <='0';
 	end if;
-	if ( (gap1_br + CONV_STD_LOGIC_VECTOR(40,11) <= bird_br) or ( bird_br + CONV_STD_LOGIC_VECTOR(136,11) <= gap1_br)) then -- turns on when bird touches outside rows
+	if ( (gap1_br + CONV_STD_LOGIC_VECTOR(80,11) <= bird_br) or ( bird_br + CONV_STD_LOGIC_VECTOR(176,11) <= gap1_br)) then -- turns on when bird touches outside rows
 		damage2 <= '1';
 	else
 		damage2 <= '0';
 	end if;	
 	
-	if( (gap2_lc <= CONV_STD_LOGIC_VECTOR(28,11)) and (CONV_STD_LOGIC_VECTOR(42,11) <= gap2_lc + CONV_STD_LOGIC_VECTOR(40,11) )) then --ga1_1c 
+	if( (gap2_lc <= CONV_STD_LOGIC_VECTOR(58,11)) and (CONV_STD_LOGIC_VECTOR(42,11) <= gap2_lc + CONV_STD_LOGIC_VECTOR(60,11) )) then --ga1_1c 
 		damage3 <='1';
 	else
 		damage3 <='0';
 	end if;
-	if ( (gap2_br + CONV_STD_LOGIC_VECTOR(40,11) <= bird_br) or ( bird_br + CONV_STD_LOGIC_VECTOR(136,11) <= gap2_br)) then -- turns on when bird touches outside rows
+	if ( (gap2_br + CONV_STD_LOGIC_VECTOR(80,11) <= bird_br) or ( bird_br + CONV_STD_LOGIC_VECTOR(176,11) <= gap2_br)) then -- turns on when bird touches outside rows
 		damage4 <= '1';
 	else
 		damage4 <= '0';
@@ -72,13 +73,13 @@ if (rising_edge(vert_sync)) then
 	f_death<= (damage2 and damage1) or(damage3 and damage4);
 	--Score functionality
 	
-	if ( (gap2_lc = CONV_STD_LOGIC_VECTOR(0,11)) or (gap1_lc = CONV_STD_LOGIC_VECTOR(0,11))  ) then
-		f_score <= f_score + conV_STD_LOGIC_VECTOR(1,7);
-		f_score_ones <= f_score_ones + "0001";	
-		if (f_score_ones > "1001") then
+	if ( ((gap2_lc = CONV_STD_LOGIC_VECTOR(50,11)) or (gap1_lc = CONV_STD_LOGIC_VECTOR(50,11))) and ((conV_STD_LOGIC_VECTOR(51,11) <= gap2_lc) or (conv_STD_LOGIC_VECTOR(51.11) < gap1_lc))) then
+		f_score <= f_score + conV_STD_LOGIC_VECTOR(1,7);	
+		f_score_ones <= f_score_ones + "0001";
+		if (f_score_ones < "1010") then
 			f_score_tens <= f_score_tens + "0001";
 			f_score_ones <= "0000";
-		elsif ((f_score_tens > "1001") and (f_score_ones > "1001")) then
+		elsif ((f_score_tens < "1010") and (f_score_ones < "1010")) then
 			f_score_tens <= "0000";
 			f_score_ones <= "0000";
 		end if;
@@ -86,13 +87,6 @@ if (rising_edge(vert_sync)) then
 end if;
 end process;
 	 
-	 
-
-		
-		
-		
-	 
-
 	 
 
 process(clk)
